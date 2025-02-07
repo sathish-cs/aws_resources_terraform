fb67a241908b6bc06d15fa67543de73f9582e5e4
Sathish Kumar C (Nokia) <sathish.kumar_c@nokia.com>
2025-02-07 18:31:32 +0530

    Updated the resource access

--- iam_users/iam-users-creation.tf
+++ iam_users/iam-users-creation.tf
@@ -10,3 +10,7 @@ resource "aws_iam_user_policy_attachment" "reader_policy" {
   user       = aws_iam_user.reader_user.name
   policy_arn = var.policy_arn
 }
+
+resource "aws_iam_access_key" "reader_access_key" {
+  user    = aws_iam_user.reader_user.name
+}
